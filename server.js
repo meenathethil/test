59d5935ae4d3c9bad23b954e5b928f5569453d21
Thomas Alexander <tomalex0@gmail.com>
2021-03-07 10:49:12 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,3 @@
+var password = "tigerpunch"
+const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlJhbmRhbGwgRGVnZ2VzIiwiaWF0IjoxNTE2MjM5MDIyfQ.sNMELyC8ohN8WF_WRnRtdHMItOVizcscPiWsQJX9hmw
+"
